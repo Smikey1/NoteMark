afb98bb622341883f4a8be22812ab04e12d8f04c
Smikey Bros <smikeywork@gmail.com>
2025-06-04 21:40:40 +1000

    :wrench: Update libs file

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -5,7 +5,8 @@ plugins {
 
     alias(libs.plugins.ksp)
     alias(libs.plugins.room)
-    alias(libs.plugins.jetbrains.kotlin.serialization)
+//    alias(libs.plugins.kotlin.serialization)
+
 }
 
 android {
@@ -50,43 +51,56 @@ android {
 
 dependencies {
 
+    // default Android
     implementation(libs.androidx.core.ktx)
     implementation(libs.androidx.lifecycle.runtime.ktx)
     implementation(libs.androidx.activity.compose)
     implementation(platform(libs.androidx.compose.bom))
-    implementation(libs.androidx.ui)
-    implementation(libs.androidx.ui.graphics)
-    implementation(libs.androidx.ui.tooling.preview)
-    implementation(libs.androidx.material3)
+    implementation(libs.androidx.compose.ui)
+    implementation(libs.androidx.compose.ui.graphics)
+    implementation(libs.androidx.compose.ui.tooling.preview)
+    implementation(libs.androidx.compose.material3)
+
+    // test
     testImplementation(libs.junit)
     androidTestImplementation(libs.androidx.junit)
     androidTestImplementation(libs.androidx.espresso.core)
     androidTestImplementation(platform(libs.androidx.compose.bom))
-    androidTestImplementation(libs.androidx.ui.test.junit4)
-    debugImplementation(libs.androidx.ui.tooling)
-    debugImplementation(libs.androidx.ui.test.manifest)
+    androidTestImplementation(libs.androidx.compose.ui.test.junit4)
+    debugImplementation(libs.androidx.compose.ui.tooling)
+    debugImplementation(libs.androidx.compose.ui.test.manifest)
 
+    // Coil
+    implementation(libs.coil.compose)
+    implementation(libs.androidx.lifecycle.viewmodel.compose)
+    implementation(libs.androidx.lifecycle.runtime.compose)
+    implementation(libs.androidx.navigation.compose)
+
+    implementation(libs.kotlinx.serialization.json)
+    implementation(libs.androidx.security.crypto.ktx)
+
+    // Koin
+    implementation(libs.bundles.koin)
+
+    //Adaptive Layout
+    implementation(libs.androidx.material3.window.size.class1)
 
     // Splashscreen & Widget
     implementation(libs.androidx.core.splashscreen)
-//    implementation(libs.bundles.widget.glance)
+
+    implementation(libs.bundles.widget.glance)
 
     // Database - Room
     implementation(libs.room.ktx)
     implementation(libs.room.runtime)
     ksp(libs.room.compiler)
 
-    // DI - Koin
-    implementation(libs.bundles.koin)
-
-    // Navigation
-    implementation(libs.androidx.compose.navigation)
-    implementation(libs.kotlinx.serialization.json)
 
     // Allow use of java.time.Instant below API 26
     coreLibraryDesugaring(libs.desugar.jdk.libs)
 
     // Logging
     implementation(libs.timber)
+
 
 }
