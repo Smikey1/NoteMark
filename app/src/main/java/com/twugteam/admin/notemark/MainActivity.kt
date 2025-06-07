71998263c148675d1254bc6b547328d43ba2c2f2
Smikey Bros <smikeywork@gmail.com>
2025-06-07 15:32:38 +1000

    :bug: Fixed Splash image bug

--- app/src/main/java/com/twugteam/admin/notemark/MainActivity.kt
+++ app/src/main/java/com/twugteam/admin/notemark/MainActivity.kt
@@ -18,8 +18,10 @@ class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
-        installSplashScreen().setKeepOnScreenCondition {
-            mainViewModel.state.isCheckingAuth
+        installSplashScreen().apply {
+            setKeepOnScreenCondition {
+                mainViewModel.state.isCheckingAuth
+            }
         }
         setContent {
             NoteMarkTheme {
