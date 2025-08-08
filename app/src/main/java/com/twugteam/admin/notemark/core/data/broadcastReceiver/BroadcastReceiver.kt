247dba1e8ba1b10521f4307ab373bc00dd56f27f
HousseinCh <houssein.ch@live.com>
2025-08-08 15:42:45 +0300

    :recycle: Refactoring BroadcastReceiver without koin injection  in it

--- app/src/main/java/com/twugteam/admin/notemark/core/data/broadcastReceiver/BroadcastReceiver.kt
+++ app/src/main/java/com/twugteam/admin/notemark/core/data/broadcastReceiver/BroadcastReceiver.kt
@@ -9,24 +9,12 @@ import androidx.work.ExistingWorkPolicy
 import androidx.work.NetworkType
 import androidx.work.OneTimeWorkRequestBuilder
 import androidx.work.WorkManager
-import com.twugteam.admin.notemark.core.data.syncing.dataSource.SyncDataSource
 import com.twugteam.admin.notemark.features.notes.constant.Constants.BROADCAST_RECEIVER_TAG
 import com.twugteam.admin.notemark.features.notes.constant.Constants.BROADCAST_RECEIVER_WORK_NAME
-import com.twugteam.admin.notemark.features.notes.constant.Constants.MANUAL_WORK_TAG
 import com.twugteam.admin.notemark.features.notes.data.workManager.SyncingWorker
-import kotlinx.coroutines.CoroutineScope
-import kotlinx.coroutines.launch
-import org.koin.compose.koinInject
-import org.koin.core.component.KoinComponent
-import org.koin.core.component.inject
 import timber.log.Timber
 
-class MyBroadcastReceiver(
-
-): BroadcastReceiver(), KoinComponent {
-    private val syncDataSource: SyncDataSource by inject()
-    private val applicationScope: CoroutineScope by inject()
-
+class MyBroadcastReceiver() : BroadcastReceiver() {
     private val constraints = Constraints.Builder()
         .setRequiredNetworkType(NetworkType.CONNECTED)
         .build()
@@ -36,23 +24,21 @@ class MyBroadcastReceiver(
     private val broadcastReceiverWorkName = BROADCAST_RECEIVER_WORK_NAME
     override fun onReceive(context: Context, intent: Intent?) {
         val workManager = WorkManager.getInstance(context)
-        Timber.tag("BroadcastReceiver").d("onReceive started: action: ${intent?.action}")
+        Timber.tag("BroadcastReceiverInGame").d("onReceive started: action: ${intent?.action}")
         if (intent?.action == ACTION_BOOT_COMPLETED) {
-            Timber.tag("BroadcastReceiver").d("action == Action_Boot_Completed")
-            applicationScope.launch {
-                Timber.tag("SyncingWorker").d("manualSync started")
-                val syncingRequest =
-                    OneTimeWorkRequestBuilder<SyncingWorker>().setConstraints(constraints)
-                        .addTag(broadcastReceiverTag).build()
-                //if constraints isn't achieved(no internet) and we call manualSync() nothing will happen
-                //but when internet is restored/turned on the workerManager will start working alone
-                //this ensure if manualSync() clicked multiple times to keep the old syncing running
-                workManager.enqueueUniqueWork(
-                    broadcastReceiverWorkName,
-                    ExistingWorkPolicy.REPLACE,
-                    syncingRequest
-                )
-            }
+            Timber.tag("BroadcastReceiverInGame").d("action == Action_Boot_Completed")
+            Timber.tag("BroadcastReceiverInGame").d("manualSync started")
+            val syncingRequest =
+                OneTimeWorkRequestBuilder<SyncingWorker>().setConstraints(constraints)
+                    .addTag(broadcastReceiverTag).build()
+            //if constraints isn't achieved(no internet) and we call manualSync() nothing will happen
+            //but when internet is restored/turned on the workerManager will start working alone
+            //this ensure if manualSync() clicked multiple times to keep the old syncing running
+            workManager.enqueueUniqueWork(
+                broadcastReceiverWorkName,
+                ExistingWorkPolicy.REPLACE,
+                syncingRequest
+            )
         }
     }
 }
